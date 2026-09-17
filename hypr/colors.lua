f2c614a61389cd568281990800cd9d4b6ff709c3
Sedly12322 <d.sedlar41@gmail.com>
2026-09-17 13:42:29 +0200

    fix(autostart): add awww-daemon, quickshell and wallpaper to autostart.lua

--- hypr/colors.lua
+++ hypr/colors.lua
@@ -1,8 +1,8 @@
 hl.config({
     general = {
         col = {
-            active_border   = { colors = { "rgba(ffb598ee)", "rgba(e7beaeee)" }, angle = 45 },
-            inactive_border = "rgba(53433e66)",
+            active_border   = { colors = { "rgba(b1c5ffee)", "rgba(c0c6dcee)" }, angle = 45 },
+            inactive_border = "rgba(44464f66)",
         },
     },
 })
--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -3,7 +3,10 @@
 -------------------
 
 hl.on("hyprland.start", function()
+    hl.exec_cmd("awww-daemon")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
+    hl.exec_cmd("quickshell")
+    hl.exec_cmd("bash " .. os.getenv("HOME") .. "/.config/hypr/scripts/set-wallpaper.sh")
 end)
